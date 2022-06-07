a5b4e2afe9080b836b6559fb22531d6caf0bcb59
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-07 15:20:33 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./components/Explore";
+import Offers from "./components/Offers";
+import Profile from "./components/Profile";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import ForgotPassword from "./components/ForgotPassword";
+
 function App() {
   return (
     <>
-      <h1>House Marketplace</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }

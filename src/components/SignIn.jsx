67f52909d10aad36783a29166f09fcd4ed1b66ac
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-07 16:52:30 -0700

    Update SignIn.jsx

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [inputData, setInputData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = inputData;
+
+  const onChange = (e) => {
+    setInputData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <>
-      <h1>Sign In</h1>
+      <div className="pageContainer">
+        <header>
+          <p>Welcome back!</p>
+        </header>
+        <main>
+          <form>
+            <input
+              type="email"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+          </form>
+        </main>
+      </div>
     </>
   );
 }

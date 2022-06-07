c4c6ff7bf87868a3efcb4d2ec01ce08c9a53b5c5
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-07 16:00:46 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,12 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg"
+import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg"
+import { ReactComponent as PersonOutline } from "../assets/svg/personOutlineIcon.svg"
+
 function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <>
       <h1>Navbar</h1>

c4c706be717ba2ee9f9903e58f3a9f5d53b56025
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-08 16:08:07 -0700

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { getAuth } from "firebase/auth";
+import { getAuth, upadateProfile } from "firebase/auth";
+import { updateDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
 
 function Profile() {
   const auth = getAuth();

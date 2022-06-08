25e6ae459c08d59df558cebaa9e45fd49c5bce70
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-08 13:40:26 -0700

    profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,16 @@
+import { useState, useEffect } from "react";
+import { getAuth } from "firebase/auth";
+
 function Profile() {
-  return (
-    <>
-      <h1>Profile</h1>
-    </>
-  );
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth();
+  useEffect(() => {
+    setUser(auth.currentUser);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return user ? <h1>{user.displayName}</h1> : <h1>not logged in</h1>;
 }
 
 export default Profile;

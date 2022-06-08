6f26b418119824fc1f79acc068aad6abde76ac45
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-07 17:05:34 -0700

    Update SignIn.jsx

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
 function SignIn() {
@@ -45,7 +47,22 @@ function SignIn() {
                 onClick={() => setShowPassword((prevState) => !prevState)}
               />
             </div>
+
+            <Link to="/forgot-password" className="forgotPasswordLink">
+              Forgot Password?
+            </Link>
+
+            <div className="signInBar">
+              <p className="signInText">Sign In</p>
+              <button className="signInButton">
+                <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+              </button>
+            </div>
           </form>
+          {/* Google OAuth */}
+          <Link to="/sign-up" className="registerLink">
+            Sign Up Instead
+          </Link>
         </main>
       </div>
     </>

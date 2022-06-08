08d832d5754bd6290243f090494398e9f3d30ff9
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-08 15:41:01 -0700

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,31 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 
 function Profile() {
-  const [user, setUser] = useState(null);
-
   const auth = getAuth();
-  useEffect(() => {
-    setUser(auth.currentUser);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
 
-  return user ? <h1>{user.displayName}</h1> : <h1>not logged in</h1>;
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={handleLogout}>
+          Log Out
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default Profile;

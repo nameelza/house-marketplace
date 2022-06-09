f46e602ad3625fcfe51cfa789e476ad0d0632a75
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 15:15:19 -0700

    Update Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,3 +1,18 @@
+import { useeffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "./Spinner";
+
 function Category() {
   return <div>Category</div>;
 }

2b536f9b81a96d3b7364d0fcf17296b326f6e92f
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 14:36:45 -0700

    Update OAth.jsx

--- src/components/OAth.jsx
+++ src/components/OAth.jsx
@@ -9,10 +9,18 @@ function OAth() {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+    } catch (error) {}
+  };
+
   return (
     <div className="socialLogin">
       <p>Sign {location.pathname === "/sign-in" ? "in" : "up"} with</p>
-      <button className="socialIconDiv">
+      <button className="socialIconDiv" onClick={onGoogleClick}>
         <img src={googleIcon} className="socialIconImg" alt="google icon" />
       </button>
     </div>

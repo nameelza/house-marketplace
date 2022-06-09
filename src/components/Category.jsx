40accb524ab54026792ed58507fc3d36e34525c7
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 15:44:58 -0700

    Update Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,4 +1,4 @@
-import { useeffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {
   collection,
@@ -14,6 +14,27 @@ import { toast } from "react-toastify";
 import Spinner from "./Spinner";
 
 function Category() {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference
+        const listingsRef = collection(db, "listings");
+        // Create query
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+      } catch (error) {}
+    };
+  });
+
   return <div>Category</div>;
 }
 

64bd2b40bee512ad72b765472917f7c85469ca72
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-08 17:23:08 -0700

    Update useAuthStatus.js

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -4,5 +4,16 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [loading, setLoading] = useState(true);
-  return null;
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setLoading(false);
+    });
+  });
+  
+  return { loggedIn, loading };
 };

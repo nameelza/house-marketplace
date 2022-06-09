6792c099bfaf30504f5f147f94cd4088c7b72758
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 15:05:54 -0700

    Update Explore.jsx

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+
 function Explore() {
   return (
-    <>
-      <h1>Explore</h1>
-    </>
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+      <main>
+        {/* Slider */}
+      </main>
+    </div>
   );
 }
 

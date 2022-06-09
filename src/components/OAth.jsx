71792a168e5fa2006b1be9495b8dccf55b7531ae
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 14:20:00 -0700

    Update OAth.jsx

--- src/components/OAth.jsx
+++ src/components/OAth.jsx
@@ -1,3 +1,10 @@
+import { useLocation, useNavigate } from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import googleIcon from "../assets/svg/googleIcon.svg";
+
 function OAth() {
   return (
     <>

50ac53e9bcde37dd9089bf07ce13be781c38631e
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-08 18:11:27 -0700

    Update ForgotPassword.jsx

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+
 function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
+  const onChange = (e) => {
+    setEmail(e.taget.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
-    <>
-      <h1>Forgot Password</h1>
-    </>
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot Password</p>
+      </header>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+        </form>
+      </main>
+    </div>
   );
 }
 

9cf02852ec5590043034f2586cac93b2ead1e17f
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 15:10:03 -0700

    Update Explore.jsx

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -10,6 +10,25 @@ function Explore() {
       </header>
       <main>
         {/* Slider */}
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+          <Link to="/category/rent">
+            <img
+              src={sellCategoryImage}
+              alt="sell"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for sale</p>
+          </Link>
+        </div>
       </main>
     </div>
   );

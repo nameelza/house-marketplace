8f6422fe968aa645a80e1ac32d194bab4886c032
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 18:30:49 -0700

    Update ListingItem.jsx

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg";
 import bedIcon from "../assets/svg/bedIcon.svg";
 import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 
-function ListingItem({ listing, id }) {
+function ListingItem({ listing, id, onDelete }) {
   console.log(listing);
   return (
     <li className="categoryListing">
@@ -46,6 +46,13 @@ function ListingItem({ listing, id }) {
           </div>
         </div>
       </Link>
+      {!onDelete && (
+        <DeleteIcon
+          className="removeIcon"
+          fill="rgb(231, 76, 60)"
+          onClick={() => onDelete(listing.id, listing.name)}
+        />
+      )}
     </li>
   );
 }

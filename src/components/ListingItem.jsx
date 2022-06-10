f4f2495900c1fc87a49f208ab84440976f6049fc
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 17:53:35 -0700

    Update ListingItem.jsx

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -6,7 +6,9 @@ import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 function ListingItem({ listing, id }) {
   return (
     <li>
-      <Link to={`/category/${listing.type}/${id}`}></Link>
+      <Link to={`/category/${listing.type}/${id}`}>
+        <img src={listing.imgURLs[0]} alt={listing.name} />
+      </Link>
     </li>
   );
 }

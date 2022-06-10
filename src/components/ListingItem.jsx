5563719a8b272c8347e2352cb6618c49bc33b749
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 18:13:46 -0700

    Update ListingItem.jsx

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -10,7 +10,22 @@ function ListingItem({ listing, id }) {
         to={`/category/${listing.type}/${id}`}
         className="categoryListingLink"
       >
-        <img src={listing.imageUrls[0]} alt={listing.name} />
+        <img
+          src={listing.imageUrls[0]}
+          alt={listing.name}
+          className="categoryListingImg"
+        />
+        <div className="categoryListingDetails">
+          <p className="categoryListingLocation">{listing.location}</p>
+          <p className="categoryListingName">{listing.name}</p>
+          <p className="categoryListingName">
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </p>
+        </div>
       </Link>
     </li>
   );

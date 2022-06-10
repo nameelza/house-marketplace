111206125f461ddaf33b5988d7ab365034d4e8ba
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 17:51:14 -0700

    Update ListingItem.jsx

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -3,8 +3,12 @@ import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg";
 import bedIcon from "../assets/svg/bedIcon.svg";
 import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 
-function ListingItem() {
-  return <div>Item</div>;
+function ListingItem({ listing, id }) {
+  return (
+    <li>
+      <Link to={`/category/${listing.type}/${id}`}></Link>
+    </li>
+  );
 }
 
 export default ListingItem;

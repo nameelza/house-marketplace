996b7f23ecedcccdc8b4afdcf2c40cdc12eedd96
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-09 17:45:35 -0700

    ListingItem

---
+++ src/components/ListingItem.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg";
+import bedIcon from "../assets/svg/bedIcon.svg";
+import bathtubIcon from "../assets/svg/bathtubIcon.svg";
+
+function ListingItem() {
+  return <div>Item</div>;
+}
+
+export default ListingItem;

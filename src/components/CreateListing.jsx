30603efd929955671ab9ba616d5c89f9803099da
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-13 11:26:46 -0700

    Update CreateListing.jsx

--- src/components/CreateListing.jsx
+++ src/components/CreateListing.jsx
@@ -1,6 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function CreateListing() {
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [formData, setFormData] = useState({
+    type: "rent",
+    name: "",
+    bedrooms: 1,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: "",
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    images: {},
+    latitude: 0,
+    longitude: 0,
+  });
   return <div>Create</div>;
 }
 

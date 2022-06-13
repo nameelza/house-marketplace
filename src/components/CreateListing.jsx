23bc36d50c08e8fc83a4b0f11fd085be18cf8eb2
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-13 16:44:55 -0700

    Update CreateListing.jsx

--- src/components/CreateListing.jsx
+++ src/components/CreateListing.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { db } from "../firebase.config";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Spinner from "./Spinner";

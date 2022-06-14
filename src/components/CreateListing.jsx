26a18f2644c7a5cb0ed00d00462d8e7507d0e76f
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-13 17:01:24 -0700

    Update CreateListing.jsx

--- src/components/CreateListing.jsx
+++ src/components/CreateListing.jsx
@@ -107,6 +107,34 @@ function CreateListing() {
 
         const storageRef = ref(storage, "images/" + fileName);
         const uploadTask = uploadBytesResumable(storageRef, image);
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+              default:
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
       });
     };
   };

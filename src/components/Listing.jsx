caf785d1371d1ddc3aa6a9d27d255f06c2208e81
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-15 14:22:24 -0700

    Update Listing.jsx

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { db } from "../firebase.config";
+import Spinner from "../components/Spinner";
+
 function Listing() {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+
+  const navigate = useNavigate();
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log(docSnap.data());
+        setListing(docSnap.data());
+      }
+    };
+
+    fetchListing();
+  }, []);
   return <div>listing</div>;
 }
 

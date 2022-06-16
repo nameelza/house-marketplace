2c96f804dd94de8fd3b1cb54e636505e31c9347d
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-15 17:15:23 -0700

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,11 @@ import { toast } from "react-toastify";
 
 function Contact() {
   const [landlord, setLandlord] = useState(null);
+  const [message, setMessage] = useState("");
 
+  const [searchParams, setSearchParams] = useSearchParams();
   const params = useParams();
+
   useEffect(() => {
     const getLandlord = async () => {
       const docRef = doc(db, "users", params.landlordId);
@@ -20,14 +23,34 @@ function Contact() {
     };
     getLandlord();
   }, [params.landlordId]);
+
   return (
     <div className="pageContainer">
       <header className="pageHeader">Contact Landlord</header>
       {landlord !== null && (
         <main>
           <div className="contactLandlord">
-            <p className="landlordName">Contact {landlord.name}</p>
+            <p className="landlordName">Contact {landlord?.name}</p>
           </div>
+
+          <form className="messageForm">
+            <div className="messageDiv">
+              <label htmlFor="message" className="messageLabel">
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="textarea"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              />
+            </div>
+            <a href={`mailto:${landlord.email}?Subject=${searchParams.get("listingName")}&body=${message}}`}>
+              <button type="button">Send message</button>
+            </a>
+          </form>
         </main>
       )}
     </div>

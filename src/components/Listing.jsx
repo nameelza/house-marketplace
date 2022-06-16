3981ea76b42eb93cae8479d33acd277d6662c055
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-15 18:14:19 -0700

    Update Listing.jsx

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { MapContainer, PopUp, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, Popup, Marker, TileLayer } from "react-leaflet";
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
@@ -89,7 +89,21 @@ function Listing() {
         </ul>
 
         <p className="listingLocationTitle">Location</p>
-        {/* map */}
+
+        <div className="leafletContainer">
+          <MapContainer
+            style={{ height: "100%", width: "100%" }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            />
+            {/* <Popup>{listing.location}</Popup> */}
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link

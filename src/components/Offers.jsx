4fcb89ad36a0c30ec0d2eff3c2ab8a6fe5b341ff
nameelza <82921161+nameelza@users.noreply.github.com>
2022-06-20 16:00:48 -0700

    Update Offers.jsx

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -30,7 +30,7 @@ function Offers() {
           listingsRef,
           where("offer", "==", true),
           orderBy("timestamp", "desc"),
-          limit(10)
+          limit(2)
         );
 
         // Execute query
@@ -39,8 +39,8 @@ function Offers() {
         const lastVisible = querySnap.docs[querySnap.docs.length - 1];
         setLastFetchedListing(lastVisible);
 
-        // Hide the button if no more listings
-        if (querySnap.docs.length <= q._query.C.limit) {
+        // Hide the button if no more listings to load
+        if (querySnap.docs.length < q._query.C.limit) {
           setLoadMoreButton(false);
         }
 
@@ -59,8 +59,51 @@ function Offers() {
         toast.error("Could not fetch listings");
       }
     };
+
     fetchListings();
   }, []);
+
+  // Pagination / Load More
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingsRef = collection(db, "listings");
+
+      // Create query
+      const q = query(
+        listingsRef,
+        where("offer", "==", true),
+        orderBy("timestamp", "desc"),
+        startAfter(lastFetchedListing),
+        limit(10)
+      );
+
+      // Execute query
+      const querySnap = await getDocs(q);
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+
+      // Hide the button if no more listings to load
+      if (querySnap.docs.length < q._query.C.limit) {
+        setLoadMoreButton(false);
+      }
+
+      const listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error("Could not fetch listings");
+    }
+  };
 
   return (
     <div className="category">
@@ -82,6 +125,11 @@ function Offers() {
                 />
               ))}
             </ul>
+            {lastFetchedListing && loadMoreButton && (
+              <p className="loadMore" onClick={onFetchMoreListings}>
+                Load more
+              </p>
+            )}
           </main>
         </>
       ) : (
